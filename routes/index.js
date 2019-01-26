b979367923acb9d36c8cd2f526ed2b59d8d876a7
Shefali Sharma <shefalisharmaspd@gmail.com>
2019-01-26 12:21:55 -0400

    add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contcat', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+
 
 module.exports = router;
